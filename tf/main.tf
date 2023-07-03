5bf6fd0971b97ea07e388f04a283b9d50c17bac6
John Poole <john@thepooles.uk>
2023-07-04 00:45:09 +0100

    making things cleverer (going for maximum effort, security and effeciency!)

--- tf/main.tf
+++ tf/main.tf
@@ -6,169 +6,81 @@ terraform {
   }
 }
 
-variable "host" {
-  type = string
+locals {
+  certs = [
+    base64decode(var.client_cert),
+    base64decode(var.client_key),
+    base64decode(var.cluster_cert),
+  ]
 }
 
+module "nginx" {
+  source = "hashicorp/kubernetes/modules/deployment"
 
-variable "client_cert" {
-  type = string
-}
+  name = "nginx"
+  namespace = "dev"
+  labels = {
+    App = "Nginx"
+  }
+  replicas = 2
+  selector = {
+    match_labels = {
+      App = "Nginx"
+    }
+  }
+  template {
+    metadata {
+      labels = {
+        App = "Nginx"
+      }
+    }
+    spec {
+      container {
+        image = "nginx"
+        name = "nginx"
 
-variable "client_key" {
-  type = string
-}
+        port {
+          container_port = 80
+        }
 
-variable "cluster_cert" {
-  type = string
-}
+        resources {
+          limits = {
+            cpu    = "0.5"
+            memory = "512Mi"
+          }
+          requests = {
+            cpu    = "250m"
+            memory = "50Mi"
+          }
+        }
+      }
+    }
+  }
 
-provider "kubernetes" {
-  host = var.host
+  service {
+    name = "nginx"
+    port {
+      node_port = 30201
+      port        = 80
+      target_port = 80
+    }
 
-  client_certificate     = base64decode(var.client_cert)
-  client_key             = base64decode(var.client_key)
-  cluster_ca_certificate = base64decode(var.cluster_cert)
-}
-
-resource "kubernetes_namespace" "dev_namespace" {
-  metadata {
-    name = "dev"
+    type = "NodePort"
   }
 }
 
 resource "kubernetes_deployment" "dev_nginx" {
-  metadata {
-    name      = "dev-nginx"
-    namespace = "dev"
-    labels = {
-      App = "DevNginx"
-    }
-  }
-
-  spec {
-    replicas = 2
-    selector {
-      match_labels = {
-        App = "DevNginx"
-      }
-    }
-    template {
-      metadata {
-        labels = {
-          App = "DevNginx"
-        }
-      }
-      spec {
-        container {
-          image = "nginx"
-          name  = "dev"
-
-          port {
-            container_port = 80
-          }
-
-          resources {
-            limits = {
-              cpu    = "0.5"
-              memory = "512Mi"
-            }
-            requests = {
-              cpu    = "250m"
-              memory = "50Mi"
-            }
-          }
-        }
-      }
-    }
-  }
+  depends_on = [module.nginx]
 }
 
 resource "kubernetes_service" "dev_nginx" {
-  metadata {
-    name      = "dev-nginx"
-    namespace = "dev"
-  }
-  spec {
-    selector = {
-      App = kubernetes_deployment.dev_nginx.spec.0.template.0.metadata[0].labels.App
-    }
-    port {
-      node_port   = 30201
-      port        = 80
-      target_port = 80
-    }
-
-    type = "NodePort"
-  }
+  depends_on = [module.nginx]
 }
 
 resource "kubernetes_deployment" "stg_nginx" {
-  metadata {
-    name      = "staging-nginx"
-    namespace = "dev"
-    labels = {
-      App = "StagingNginx"
-    }
-  }
-
-  spec {
-    replicas = 2
-    selector {
-      match_labels = {
-        App = "StagingNginx"
-      }
-    }
-    template {
-      metadata {
-        labels = {
-          App = "StagingNginx"
-        }
-      }
-      spec {
-        container {
-          image = "nginx"
-          name  = "staging"
-
-          port {
-            container_port = 80
-          }
-
-          resources {
-            limits = {
-              cpu    = "0.5"
-              memory = "512Mi"
-            }
-            requests = {
-              cpu    = "250m"
-              memory = "50Mi"
-            }
-          }
-        }
-      }
-    }
-  }
+  depends_on = [module.nginx]
 }
 
 resource "kubernetes_service" "stg_nginx" {
-  metadata {
-    name      = "staging-nginx"
-    namespace = "dev"
-  }
-  spec {
-    selector = {
-      App = kubernetes_deployment.stg_nginx.spec.0.template.0.metadata[0].labels.App
-    }
-    port {
-      node_port   = 30202
-      port        = 80
-      target_port = 80
-    }
-
-    type = "NodePort"
-  }
-}
-
-module nginx-deploymnents {
-  source = "./nginx-deployments.tf"
+  depends_on = [module.nginx]
 }
--- tf/modules/kubernetes-deployment/output.tf
+++
@@ -1,3 +0,0 @@
-output selector {
-    value = kubernetes_deployment.nginx.spec.0.template.0.metadata[0].labels.App
-}
